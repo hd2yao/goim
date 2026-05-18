fb3c865bec7421507b75fa3c6eb035055e1b5e8c
hd2yao <1054290355@qq.com>
2026-05-19 00:34:10 +0800

    feat: go-zero api & rpc 示例

---
+++ user/rpc/internal/logic/getuserlogic.go
@@ -0,0 +1,31 @@
+package logic
+
+import (
+	"context"
+
+	"github.com/hd2yao/goim/user/rpc/internal/svc"
+	"github.com/hd2yao/goim/user/rpc/user"
+
+	"github.com/zeromicro/go-zero/core/logx"
+)
+
+type GetUserLogic struct {
+	ctx    context.Context
+	svcCtx *svc.ServiceContext
+	logx.Logger
+}
+
+func NewGetUserLogic(ctx context.Context, svcCtx *svc.ServiceContext) *GetUserLogic {
+	return &GetUserLogic{
+		ctx:    ctx,
+		svcCtx: svcCtx,
+		Logger: logx.WithContext(ctx),
+	}
+}
+
+// 获取用户信息
+func (l *GetUserLogic) GetUser(in *user.GetUserReq) (*user.GetUserResp, error) {
+	// todo: add your logic here and delete this line
+
+	return &user.GetUserResp{}, nil
+}
---
+++ user/rpc/internal/svc/servicecontext.go
@@ -0,0 +1,13 @@
+package svc
+
+import "github.com/hd2yao/goim/user/rpc/internal/config"
+
+type ServiceContext struct {
+	Config config.Config
+}
+
+func NewServiceContext(c config.Config) *ServiceContext {
+	return &ServiceContext{
+		Config: c,
+	}
+}
